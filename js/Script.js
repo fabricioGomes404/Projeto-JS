3e90028c0a9c278bab18ccea1f75b915cf6eec7b
fabricioGomes404 <fabricio.pereiraa18@gmail.com>
2026-03-30 13:42:17 -0300

    aa

--- js/Script.js
+++ js/Script.js
@@ -2,6 +2,7 @@ function calcular() {
     let nota1 = parseFloat(document.getElementById("nota1").value);
     let nota2 = parseFloat(document.getElementById("nota2").value);
     let media = (nota1 + nota2) / 2;
-    alert("A média é: " + media);
+    let resultado = document.getElementById("resultado");
+    resultado.textContent = `media: ${media.toFixed(1)}`
+
 }
- 
