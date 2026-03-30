cf6568344d3810936e60eeae146f7aa40ad3cbc1
fabricioGomes404 <fabricio.pereiraa18@gmail.com>
2026-03-30 14:05:44 -0300

    a

--- js/Script.js
+++ js/Script.js
@@ -1,8 +1,17 @@
 function calcular() {
     let nota1 = parseFloat(document.getElementById("nota1").value);
     let nota2 = parseFloat(document.getElementById("nota2").value);
-    let media = (nota1 + nota2) / 2;
+    let nota3 = parseFloat(document.getElementById("nota3").value);
+    let media = (nota1 + nota2 + nota3) / 3;
+    let nome = document.getElementById("nome").value;
+    let mensagem = document.getElementById("mensagem");''
     let resultado = document.getElementById("resultado");
-    resultado.textContent = `media: ${media.toFixed(1)}`
-
+    resultado.textContent = `ola ${nome}, sua média é: ${media.toFixed(1)}`
+    
+    if (media >= 6) { 
+        mensagem.textContent = `Você foi aprovado.`;
+    } else {
+        mensagem.textContent = `Você foi reprovado.`;
+    }
 }
+
